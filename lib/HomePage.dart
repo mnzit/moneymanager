b0fb587332037f7a46442e411126137a02d8fa25
mnzit <mnzitshakya@gmail.com>
2023-04-22 13:44:23 +0545

    init

---
+++ lib/HomePage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = _counter + 1;
+    });
+    debugPrint("_counter: $_counter");
+  }
+
+
+//  This method is rerun every time setState is called, for instance as done by the _incrementCounter method above
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 100)
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
